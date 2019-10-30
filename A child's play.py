cff84e6b20b2a23ca1d8b5dbe15255994d245100
MarcoBarion <marco.barion94@gmail.com>
2019-10-30 16:58:48 +0100

    Update A child's play.py

--- A child's play.py
+++ A child's play.py
@@ -12,23 +12,62 @@ def find_initial_position(maze, w, h):
     
     # For now it returns -1 try to implement with exceptions
     return -1
-    
-def can_move(maze, pos_x, pos_y):
-    if maze[pos_x][pos_y] == "#":
+
+# INPUT:
+# maze: list 2 dimensions
+# coordinates: list 2 values
+def can_move(coordinates, direction):
+    tmp_coordinates = coordinates[:]
+    tmp_coordinates = move(direction, tmp_coordinates)
+    if maze[ tmp_coordinates[0] ][ tmp_coordinates[1] ] == "#":
         return False
     
-    returt True
+    return True
 
-def move(maze, direction):
-    move_dic = 
+def turn_clockwise(direction):
+    directions = ["U", "R", "D", "L"]
+    return directions[ (directions.index(direction) + 1) % 4]
+
+# INPUT:
+# maze
+# direction
+# coordinates
+#
+# OUTPUT:
+# coordinates: list of coordinates
+#
+# N.B. there is probably no need of a return, when you modify coordinates this already modify the object in memory
+def move(direction, coordinates):
+    if direction == "U":
+        coordinates[0] -=1
+    elif direction == "R":
+        coordinates[1] +=1
+    elif direction == "D":
+        coordinates[0] +=1
+    elif direction == "L":
+        coordinates[1] -=1
+    
+    return coordinates
+    
 
 w, h = [int(i) for i in input().split()]
 n = int(input())
 
 maze = []
+direction = "U"
+position = [0, 0]
 
 for i in range(h):
     line = input()
     maze.append(line)
     
-print(maze)
+position = find_initial_position(maze, w, h)
+
+for i in range(0, n):
+    # We are not taking care of the case that the guy can not rotate more than once
+    if not can_move(position, direction):
+        turn_clockwise(direction)
+    position = move(direction, position)
+
+print( "%(x)d %(y)d" %{"x": position[0], "y": position[1]} )
+
